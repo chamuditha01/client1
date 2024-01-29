d8b7afc62b922ce73dfe4f812a75ca8c2ee0dd65
chamuditha01 <heshanchamuditha05@gmail.com>
2024-01-29 17:50:24 +0530

    ca1

--- src/Carousel/CarouselPage.jsx
+++ src/Carousel/CarouselPage.jsx
@@ -37,56 +37,50 @@ function CarouselPage() {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-         
-
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-  <NavItems NavItem id={"nav1"} name={'Home'} path={'/'} />
-  <NavItems NavItem id={"nav1"} name={'About Us'} path={'/service'} />
-  <li className="nav-item dropdown ">
-    <a
-      className="nav-link dropdown-toggle "
-      href="#"
-      id="nav1"
-      role="button"
-      data-bs-toggle="dropdown"
-      aria-haspopup="true"
-      aria-expanded="false"
-    >
-      Categories
-    </a>
-    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-      <a className="dropdown-item" href="/category1">
-      Cloths
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronicss
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      <a className="dropdown-item" href="/category2">
-        Electronics
-      </a>
-      {/* Add more categories as needed */}
-    </div>
-  </li>
-</ul>
-
-
-
+          <NavItems NavItem id={"nav1"} name={'Home'} path={'/'} />
+          <NavItems NavItem id={"nav1"} name={'About Us'} path={'/service'} />
+          <li className="nav-item dropdown ">
+            <a
+              className="nav-link dropdown-toggle "
+              href="#"
+              id="nav1"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Categories
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="/category1">
+              Cloths
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronicss
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              <a className="dropdown-item" href="/category2">
+                Electronics
+              </a>
+              {/* Add more categories as needed */}
+            </div>
+          </li>
+        </ul>
             <ul className="navbar-nav mb-2 mb-lg-0 right">
               <NavItems NavItem id={"nav"} name={'Contact Us'} path={'/login'} />
               <a className="top" href="/login">
