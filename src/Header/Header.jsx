d29a9be9fe4e1ed7a95d80d2b8483242f18e5e12
Akila Nirmal <akilanirmalzz4352@gmail.com>
2024-02-03 10:27:15 +0530

    delete card

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Header.css';
 import Navbar from "../Navbar";
 import CarouselPage from "../Carousel/CarouselPage";
-import CardPage from "../Card/CardPage";
+
 import AboutUs from "../Aboutus/AboutUs";
 
 
@@ -14,7 +14,7 @@ const Header = () => {
     
     >
       <div className="container2">
-        <CarouselPage/><AboutUs/><CardPage/>
+        <CarouselPage/><AboutUs/>
         <div className="header__content text__center text__light flex flex__center">
       
         </div>
