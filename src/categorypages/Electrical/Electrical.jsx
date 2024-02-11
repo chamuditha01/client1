0c01668138eb1b71d479df0461188f9bcaad1342
chamuditha01 <heshanchamuditha05@gmail.com>
2024-02-11 10:56:26 +0530

    21

--- src/categorypages/Electrical/Electrical.jsx
+++ src/categorypages/Electrical/Electrical.jsx
@@ -2,7 +2,7 @@ import Navbar1 from '../../Navbar';
 import Footer from '../../Footer/Footer';
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
-import img5 from './full-length-woman-standing-kitchen_1048944-13021513.jpg';
+import img5 from './electric-circuit-breaker-isolated-white-background-electric-line-protection_113913-1574.jpg';
 import img6 from './blue-sunglasses-reflect-tropical-summer-fun-elegance-generated-by-ai_188544-10320.avif';
 import img7 from './photo-top-view-huge-set-collection-working-hand-power-tools-many-wooden-isolated-black-surface_185126-50.avif';
 import img8 from './tools-instruments-belt-black-background_182252-6921.avif'
@@ -19,7 +19,7 @@ import Adaptor from "./AC DC Adaptor Input 100-220VAC Out Put 6VDVC 1A.jpg";
     { id: 2, src: Resistant, title: '10mm Heat Resistant Fiberglass Sleeve    ' },
     { id: 3, src: Heat, title: '8mm Heat Shrink Sleeve    '},
     { id: 1, src: Adaptor, title: 'AC DC Adaptor Input 100-220VAC Out Put 6VDVC 1A    ' },
-    { id: 2, src: img5, title: 'House Keeping, Laundry & Kitchen '},
+    { id: 2, src: img5, title: '110V Contactor LS mc18b'},
     { id: 1, src: img6, title: 'Marine, Cold room, Swimming Pool' },
     { id: 3, src: img7, title: 'Tools and Maintenance related' },
     { id: 2, src: img8, title: 'Others' }
