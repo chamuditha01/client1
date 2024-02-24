88ba7ad55614a9b842c405e88effbeac0889db6d
chamuditha01 <heshanchamuditha05@gmail.com>
2024-02-24 19:09:45 +0530

    s1

--- src/SocialIcons/index.jsx
+++ src/SocialIcons/index.jsx
@@ -17,7 +17,7 @@ const SocialIcons = () => {
       <a id="s1" href="tel:+94769673620"><MdAddIcCall size={30} color='black' /></a>
       </div>
       <div style={{ marginBottom: '10px', backgroundColor:'#0de2f1' , padding:'2px',borderRadius:'2px' }}>
-      <a id="s1" href="https://www.linkedin.com/in/fahad-lahir-360368140?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><IoLogoLinkedin size={30} color='black' /></a>
+      <a id="s1" href="https://www.linkedin.com/in/mohamed-fahadh-360b25142?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><IoLogoLinkedin size={30} color='black' /></a>
     </div>
     </div>
   );
